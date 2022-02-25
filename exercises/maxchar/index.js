8a62ebb0f5731b4cca02a74684de943ccc995784
Manlangit <manlangitv@gmail.com>
2022-02-25 07:32:31 -0500

    maxChar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,24 +6,33 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-    const characterMap = {};
-    let max = 0;
-    let maxChar = '';
+   //creating an object to place/adding each characters of the string
+   const charMap = {};
+   //placeholder for counting the maximum value
+   let max = 0;
+   //placeholder for the character that been found the most
+   let maxChar = '';
 
-for(let char of str){
-   if(characterMap[char]){
-        characterMap[char]++
-    } else {
-        characterMap[char] = 1;
-    } 
-  }  
-  for (let char in characterMap) {
-      if(characterMap[char]>max){
-          max = characterMap[char];
-          maxChar = char;
+   //'for of' loop iterating in an array or a string or iterable object
+   for (let character of str) {
+      //Goal is to add 'character' to object 'characters'
+      //characters[character] will referrence to the value of 'character' and add it to 'characters'
+      //if nothing exist right now, set the value to 1 otherwise if there's value exist increment to 1
+      if (!charMap[character]) {
+         charMap[character] = 1;
+      } else {
+         charMap[character]++;
       }
-  }
-  return maxChar;
+   }
+   //'for in' iterating on actual object or javascript of object
+   //'char' represents the keys not the values
+   for (let char in charMap) {
+      if (charMap[char] > max) {
+         max = charMap[char];
+         maxChar = char;
+      }
+   }
+   return maxChar;
 }
 
 module.exports = maxChar;
