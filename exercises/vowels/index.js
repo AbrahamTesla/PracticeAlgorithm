d3eef9a4a375aa9940bb2f9e93cab73ac733fbd3
Manlangit <manlangitv@gmail.com>
2022-03-07 22:38:32 -0500

    vowel checker

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,39 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//    const vowels = [];
+//    let word = str.toLowerCase().split('');
 
+//    for (let letter of word) {
+//       if (
+//          letter === 'a' ||
+//          letter === 'i' ||
+//          letter === 'e' ||
+//          letter === 'o' ||
+//          letter === 'u'
+//       )
+//          vowels.push(letter);
+//    }
+//    if (vowels === null) {
+//       return 0;
+//    } else {
+//       return vowels.length;
+//    }
+// }
+
+//Using the 'includes' method & a counter
+function vowels(str) {
+   let count = 0;
+   //Declare a variable 'checker' to check if this vowel exist 'aieou'
+   const checker = ['a', 'e', 'i', 'o', 'u'];
+
+   for (let letter of str.toLowerCase()) {
+      //checker includes the current character or letter then increment 'count by 1'
+      if (checker.includes(letter)) {
+         count++;
+      }
+   }
+   return count;
+}
 module.exports = vowels;
